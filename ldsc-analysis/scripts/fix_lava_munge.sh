fbbb11d9682c40da0bd4b561ab836a0586aa2838
le-chang <le.chang@umontreal.ca>
2025-10-08 15:01:52 -0400

    added merge allele in munge

--- ldsc-analysis/scripts/fix_lava_munge.sh
+++ ldsc-analysis/scripts/fix_lava_munge.sh
@@ -13,6 +13,8 @@ module load apptainer/1.3.5
 
 SIF_FILE="/home/lchang24/projects/def-gsarah/lchang24/github/nf-genetic-correlations_fork/nf-genetic-correlations/bin/ldsc_latest.sif"
 OUTPUT_DIR="/home/lchang24/scratch/ldsc_munged"
+SNP_LIST_PATH="/home/lchang24/projects/def-gsarah/lchang24/github/nf-genetic-correlations_fork/nf-genetic-correlations/data/ld_reference/w_hm3.snplist"
+
 mkdir -p ${OUTPUT_DIR}
 
 # ============================================================================
@@ -26,6 +28,7 @@ echo "Processing PD_females - normal orientation..."
 apptainer run -W ${SLURM_TMPDIR} ${SIF_FILE} munge_sumstats.py \
     --sumstats /home/lchang24/projects/def-gsarah/sumstats/kidney_neurodegen/PD_females.lava.gz \
     --out ${OUTPUT_DIR}/PD_females_normal \
+    --merge-alleles ${SNP_LIST_PATH} \
     --N 104082 \
     --ignore N \
     --snp SNP \
@@ -38,6 +41,7 @@ echo "Processing PD_females - flipped orientation..."
 apptainer run -W ${SLURM_TMPDIR} ${SIF_FILE} munge_sumstats.py \
     --sumstats /home/lchang24/projects/def-gsarah/sumstats/kidney_neurodegen/PD_females.lava.gz \
     --out ${OUTPUT_DIR}/PD_females_flipped \
+    --merge-alleles ${SNP_LIST_PATH} \
     --N 104082 \
     --ignore N \
     --snp SNP \
@@ -53,6 +57,7 @@ echo "Processing PD_males - normal orientation..."
 apptainer run -W ${SLURM_TMPDIR} ${SIF_FILE} munge_sumstats.py \
     --sumstats /home/lchang24/projects/def-gsarah/sumstats/kidney_neurodegen/PD_males.lava.gz \
     --out ${OUTPUT_DIR}/PD_males_normal \
+    --merge-alleles ${SNP_LIST_PATH} \
     --N 110616 \
     --ignore N \
     --snp SNP \
@@ -65,6 +70,7 @@ echo "Processing PD_males - flipped orientation..."
 apptainer run -W ${SLURM_TMPDIR} ${SIF_FILE} munge_sumstats.py \
     --sumstats /home/lchang24/projects/def-gsarah/sumstats/kidney_neurodegen/PD_males.lava.gz \
     --out ${OUTPUT_DIR}/PD_males_flipped \
+    --merge-alleles ${SNP_LIST_PATH} \
     --N 110616 \
     --ignore N \
     --snp SNP \
@@ -80,6 +86,7 @@ echo "Processing PDnp_females - normal orientation..."
 apptainer run -W ${SLURM_TMPDIR} ${SIF_FILE} munge_sumstats.py \
     --sumstats /home/lchang24/projects/def-gsarah/sumstats/kidney_neurodegen/PDnp_females.lava.gz \
     --out ${OUTPUT_DIR}/PDnp_females_normal \
+    --merge-alleles ${SNP_LIST_PATH} \
     --N 19773 \
     --ignore N \
     --snp SNP \
@@ -92,6 +99,7 @@ echo "Processing PDnp_females - flipped orientation..."
 apptainer run -W ${SLURM_TMPDIR} ${SIF_FILE} munge_sumstats.py \
     --sumstats /home/lchang24/projects/def-gsarah/sumstats/kidney_neurodegen/PDnp_females.lava.gz \
     --out ${OUTPUT_DIR}/PDnp_females_flipped \
+    --merge-alleles ${SNP_LIST_PATH} \
     --N 19773 \
     --ignore N \
     --snp SNP \
@@ -107,6 +115,7 @@ echo "Processing PDnp_males - normal orientation..."
 apptainer run -W ${SLURM_TMPDIR} ${SIF_FILE} munge_sumstats.py \
     --sumstats /home/lchang24/projects/def-gsarah/sumstats/kidney_neurodegen/PDnp_males.lava.gz \
     --out ${OUTPUT_DIR}/PDnp_males_normal \
+    --merge-alleles ${SNP_LIST_PATH} \
     --N 24053 \
     --ignore N \
     --snp SNP \
@@ -119,6 +128,7 @@ echo "Processing PDnp_males - flipped orientation..."
 apptainer run -W ${SLURM_TMPDIR} ${SIF_FILE} munge_sumstats.py \
     --sumstats /home/lchang24/projects/def-gsarah/sumstats/kidney_neurodegen/PDnp_males.lava.gz \
     --out ${OUTPUT_DIR}/PDnp_males_flipped \
+    --merge-alleles ${SNP_LIST_PATH} \
     --N 24053 \
     --ignore N \
     --snp SNP \
